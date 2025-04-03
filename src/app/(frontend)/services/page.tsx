3e29167504b0e5d0f0b424df8466c6922e60ad92
masonomara-vs <mason@vemarasolutions.com>
2025-04-03 14:18:09 -0400

    added type to post

--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -2,22 +2,30 @@ import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
 
+// Define a type for the Post
+interface Post {
+  _id: string;
+  title: string;
+  slug: {
+    current: string;
+  };
+}
+
 export default async function Page() {
-  const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
+  const { data: posts }: { data: Post[] } = await sanityFetch({ query: POSTS_QUERY });
 
   return (
     <main>
-      <h1>post index</h1>
+      <h1>Post Index</h1>
       <ul>
         {posts.map((post) => (
           <li key={post._id}>
-            <Link href={`/services/${post?.slug?.current}`}
-            >
+            <Link href={`/services/${post?.slug?.current}`}>
               {post?.title}
             </Link>
           </li>
         ))}
       </ul>
     </main>
-  )
+  );
 }
