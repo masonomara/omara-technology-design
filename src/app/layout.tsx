5347e917a75477ffb65378a1b29cea513f4b26ae
masonomara-vs <mason@vemarasolutions.com>
2025-04-03 14:34:17 -0400

    reworked the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 
+import { Overpass } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "O'Mara Technology & Design",
@@ -7,14 +8,21 @@ export const metadata: Metadata = {
     "Digital Product Strategy, Design, and Development - Mobile Apps, Websites, E-Commerce",
 };
 
+const overpass = Overpass({
+  subsets: ["latin"],
+  display: "swap",
+  variable: '--font-overpass',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" style={{ margin: "0px", backgroundColor: "#EDE1CC" }}>
+      <body className={` ${overpass.variable}`} style={{ margin: "0px", position: "relative" }}>
+        {children}</body>
     </html>
   )
 }
