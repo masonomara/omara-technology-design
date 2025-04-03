de2d5b16f619330fb804cabccc04dcaf395c6772
masonomara-vs <mason@vemarasolutions.com>
2025-04-03 14:16:35 -0400

    added config for images

---
+++ src/app/(frontend)/services/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import { sanityFetch } from "@/sanity/lib/live";
+import { POST_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
+import { PortableText } from "next-sanity";
+import { components } from "@/sanity/portableTextComponents";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { data: post } = await sanityFetch({
+    query: POST_QUERY,
+    params: await params,
+  });
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <main>
+      {post?.mainImage ? (
+        <Image
+          src={urlFor(post.mainImage)
+            .width(800)
+            .height(300)
+            .quality(80)
+            .auto("format")
+            .url()}
+          alt={post?.mainImage?.alt || ""}
+          width="800"
+          height="300"
+        />
+      ) : null}
+      <h1>{post?.title}</h1>
+      {post?.body ? (
+        <div>
+          <PortableText value={post.body} components={components} />
+        </div>
+      ) : null}
+      <hr />
+      <Link href="/posts">&larr; Return to index</Link>
+    </main>
+  );
+}
---
+++ src/sanity/portableTextComponents.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { PortableTextComponents } from "next-sanity";
+import { urlFor } from "@/sanity/lib/image";
+
+export const components: PortableTextComponents = {
+  types: {
+    image: (props) =>
+      props.value ? (
+        <Image
+          src={urlFor(props.value)
+            .width(600)
+            .height(400)
+            .quality(80)
+            .auto("format")
+            .url()}
+          alt={props?.value?.alt || ""}
+          width="600"
+          height="400"
+        />
+      ) : null,
+  },
+};
