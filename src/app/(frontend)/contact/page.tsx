161104feeec0072989a14e80ad35909820b6528c
masonomara-vs <mason@vemarasolutions.com>
2025-04-07 08:29:19 -0400

    started cotnact form index

---
+++ src/app/(frontend)/contact/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Contacts() {
+  return (
+    <div>
+      <div style={{ fontFamily: "serif" }}>Contact</div>
+      <div style={{ fontFamily: "sans-serif" }}>To work with O'Mara Tech & Design as a client, press here</div>
+      <div>
+        <div>
+          For press Inquiries
+        </div>
+        <div>
+          For job inquiries
+        </div>
+        <div>
+          For other inquiries
+        </div>
+        <div>
+          Or if contact forms arent your thing, jsut email mason@omaratechnologydesign.com
+        </div>
+        <div style={{ fontFamily: "serif" }}>
+          O'Mara Technology & Design
+        </div>
+      </div>
+    </div>
+  )
+}
