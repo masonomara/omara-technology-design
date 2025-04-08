32bee36a2e969ec0a09c9a576356f696ba94c990
masonomara-vs <mason@vemarasolutions.com>
2025-04-08 13:21:47 -0400

    brain fart stuff

--- src/app/components/Marquee.tsx
+++ src/app/components/Marquee.tsx
@@ -32,18 +32,37 @@ export default function Marquee() {
       <div className="menu__navigation">
         <Link id={"footerHome"} className={"footer__menuOption"} href="/" target="_top">
           <span>Home</span>
+          <span>Beat your high score in our shooter, 
+Vemara Solutions
+Drive your business towards goals and new opportunities.
+
+Access industry-leading technology strategy, design, and development for digital products and solutions.</span>
         </Link>
         <Link id={"footerWork"} className={"footer__menuOption"} href="/work" target="_top">
           <span>Work</span>
+          <span>Creating intuitive, creative, and scalable solutions with industry-leading technology.
+
+We have over 20 active clients with an average relationship of more than a year and a 96% retention rate.</span>
+
         </Link>
         <Link id={"footerServices"} className={"footer__menuOption"} href="/services" target="_top">
           <span>Services</span>
+          <span>Working within a variety of business contexts for your needs. Proud to deliver a diverse range of services, solutions, technologies, and business contexts to meet our client‘s business needs. Flexible and scalable solutions.</span>
+
         </Link>
         <Link id={"footerAbout"} className={"footer__menuOption"} href="/about" target="_top">
           <span>About</span>
+          <span>Working within a variety of business contexts for your needs. From digital products including mobile apps, websites, and custom software to technical solutions.
+
+Our team drives businesses towards their goals and create new opportunities through our comprehensive services. </span>
+
         </Link>
         <Link id={"footerContact"} className={"footer__menuOption"} href="/contact" target="_top">
           <span>Contact</span>
+          <span>We're excited to see what comes next.
+
+We like to partner with businesses that are leading innovative initiatives and using industry-leading technology to drive their goals forward.</span>
+
         </Link>
       </div>
     </div>
