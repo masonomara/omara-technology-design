e51243004497f0d8cdf935169cb301e946afb923
masonomara-vs <mason@vemarasolutions.com>
2025-04-08 07:10:08 -0400

    got contact apge working

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -13,33 +13,33 @@ export default function Contacts() {
 
 
           <Link href="mailto:contact@omaratechnologydesign.com" target="_blank" className={`${styles.buttonBorder} ${styles.mainButton}`}>
-
-            <div className={styles.buttonHeading}>Letæs Get Started</div>
-            <div className={styles.buttonSubheading}>Work with O‘Mara Technology & Design</div>
-
-
+            <div className={styles.buttonHeading}>FOR OPPORTUNITIES TO WORK WITH O.T.D<br />PLEASE click here</div>
+            {/* <div className={styles.buttonSubheading}>PLEASE CLICK HERE</div> */}
           </Link>
 
           <div className={styles.subButtonsWrapper}>
             <Link href="mailto:contact@omaratechnologydesign.com" target="_blank" className={`${styles.buttonBorder} ${styles.subButton}`}>
-              Press Inquiries
+              For Press Inquiries<br />
+              CLICK HERE
             </Link>
             <Link href="mailto:contact@omaratechnologydesign.com" target="_blank" className={`${styles.buttonBorder} ${styles.subButton}`}>
-              Join Our Team
+              FOR JOB INQUIRIES<br />
+              CLICK HERE
             </Link>
             <Link href="mailto:contact@omaratechnologydesign.com" target="_blank" className={`${styles.buttonBorder} ${styles.subButton}`}>
-              General Inquiries
+              FOR GENERAL INQUIRIES<br />
+              CLICK HERE
             </Link>
           </div>
           <div className={styles.emailInfo}>
-            Online forms aren‘t your thing? Feel free to email <Link className={styles.emailLink} href="mailto:contact@omaratechnologydesign.com" target="_blank">contact@omaratechnologydesign.com</Link>.
+            COntact forms not your thing? Feel free to email <Link className={styles.emailLink} href="mailto:contact@omaratechnologydesign.com" target="_blank">contact@omaratechnologydesign.com</Link>
           </div>
         </div>
 
 
 
         <div className={styles.companyTitle}>
-          O‘Mara Technology & Design
+          O’Mara Technology & Design
         </div>
       </div>
     </div>
