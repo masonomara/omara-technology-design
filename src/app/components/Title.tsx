8c62488452118f2f3a68898c770e825d9fd7da3f
masonomara <mason.omara@gmail.com>
2026-04-09 10:42:03 -0500

    portfolio: delete all sanity infrastructure

--- src/app/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export function Title(props: PropsWithChildren) {
-  return (
-    <h1>
-      {props.children}
-    </h1>
-  )
-}
--- src/sanity/lib/fetchRedirects.ts
+++
@@ -1,6 +0,0 @@
-import { client } from "./client";
-import { REDIRECTS_QUERY } from "./queries";
-
-export async function fetchRedirects() {
-  return client.fetch(REDIRECTS_QUERY);
-}
