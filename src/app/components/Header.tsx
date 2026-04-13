3573df2aeb2409118e6ca4eae3879e7c03559073
masonomara <mason.omara@gmail.com>
2026-04-13 07:05:39 -0600

    header: polished header styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,89 +1,45 @@
-// components/Header.tsx
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useMenu } from "../context/MenuContext";
+
+const NAV_LINKS = [
+  { id: "home", label: "Home", href: "/" },
+  { id: "work", label: "Work", href: "/work" },
+  { id: "about", label: "About", href: "/about" },
+  { id: "process", label: "Process", href: "/process" },
+  { id: "contact", label: "Contact", href: "/contact" },
+];
 
 export default function Header() {
-  const { menuOpen, setMenuOpen } = useMenu();
-
-  // Toggle menu and update state
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <header className={"header"}>
-      <div className={"header__content"}>
-        <div className={"header__content--left"}>
-          <Link href="/" className={"header__logoWrapper"}>
+    <header className="header">
+      <div className="header__content">
+        <div className="header__content--left">
+          <Link href="/" className="header__logoWrapper">
             <Image
-              className={"header__logo--desktop"}
-              src="/monogramText.svg"
-              width={158}
-              height={51}
+              className="header__logo--desktop"
+              src="/longWordmark.svg"
+              width={326}
+              height={20.03}
               alt="Logo"
             />
             <Image
-              className={"header__logo--mobile"}
-              src="/top-logo.svg"
+              className="header__logo--mobile"
+              src="/top-logo-spacing.svg"
               width={326}
               height={51.03}
               alt="O'Mara Technology"
             />
           </Link>
-          <div className={"header__textContainer"}>
-            {/* <div id={"textAbout"} className={"header__textWrapper"}>
-              <div className={"header__textTitle"}>Digital Product Agency</div>
-
-              <div className={"header__textBody"}>
-                Product design and technical
-              </div>
-              <div className={"header__textBody"}>
-                development strategy and services
-              </div>
-            </div> */}
-            {/* <div id={"textWhere"} className={"header__textWrapper"}>
-              <div className={"header__textTitle"}>Where</div>
-              <div className={"header__textBody"}>
-                1301 Corlies Ave Suite 2D,
-              </div>
-              <div className={"header__textBody"}>Asbury Park, NJ 07712</div>
-            </div> */}
-            {/* <div id={"textFocuses"} className={"header__textWrapper"}>
-              <div className={"header__textTitle"}>Focuses</div>
-              <div className={"header__textBody"}>
-                Mobile Apps&nbsp;&nbsp;&nbsp;Websites
-              </div>
-              <div className={"header__textBody"}>Software</div>
-            </div> */}
-          </div>
+          <div className="header__textContainer" />
         </div>
-        <div className={"header__content--right"}>
-          <div className={"header__menuWrapper"}>
-            <Link id={"work"} className={"header__menuOption"} href="/work">
-              <span>Work</span>
-            </Link>
-            <Link id={"about"} className={"header__menuOption"} href="/about">
-              <span>About</span>
-            </Link>
-            <Link
-              id={"process"}
-              className={"header__menuOption"}
-              href="/process"
-            >
-              <span>Process</span>
-            </Link>
-
-            <Link
-              id={"contact"}
-              className={"header__menuOption"}
-              href="/contact"
-            >
-              <span>Contact</span>
-            </Link>
-          </div>
+        <div className="header__content--right">
+          <nav className="header__menuWrapper">
+            {NAV_LINKS.map(({ id, label, href }) => (
+              <Link key={id} id={id} className="header__menuOption" href={href}>
+                <span>{label}</span>
+              </Link>
+            ))}
+          </nav>
         </div>
       </div>
     </header>
